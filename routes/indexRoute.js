6267340546e65468367ff81daa4bfed9863155df
Kummirai <ajaxmilton@hotmail.com>
2024-11-10 18:26:21 +0200

    Install body parser

--- routes/indexRoute.js
+++ routes/indexRoute.js
@@ -17,11 +17,20 @@ const messages = [
   },
 ];
 
+app.use(express.urlencoded({ extended: true }));
+
 app.set("views", path.join(__dirname, "views"));
 app.set("views engine", "ejs");
 
 indexRoute.get("/", (req, res) => {
   res.render("index.ejs", { title: "Mini Message Board", messages: messages });
+});
+
+indexRoute.post("/new", (req, res) => {
+  const usr = req.body.name;
+  const msg = req.body.message;
+  messages.push({ text: msg, user: usr });
+  res.redirect("/");
 });
 
 module.exports = indexRoute;
--- routes/newRoute.js
+++ routes/newRoute.js
@@ -1,8 +1,14 @@
+const express = require("express");
+const app = express();
 const { Router } = require("express");
 const newRoute = Router();
+const path = require("node:path");
+
+app.set("views", path.join(__dirname, "views"));
+app.set("views engine", "ejs");
 
 newRoute.get("/", (req, res) => {
-  res.send("This is a new route");
+  res.render("form.ejs");
 });
 
 module.exports = newRoute;
