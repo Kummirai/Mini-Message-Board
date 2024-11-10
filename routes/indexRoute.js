c537b45f1b86be6195672628f09c681b94692de2
Kummirai <ajaxmilton@hotmail.com>
2024-11-10 17:43:23 +0200

    Add message board title

--- routes/indexRoute.js
+++ routes/indexRoute.js
@@ -1,9 +1,8 @@
+const express = require("express");
+const app = express();
 const { Router } = require("express");
 const indexRoute = Router();
 const path = require("node:path");
-
-app.set("views", path.join(__dirname, "views"));
-app.set("views engine", "ejs");
 
 const messages = [
   {
@@ -18,8 +17,11 @@ const messages = [
   },
 ];
 
+app.set("views", path.join(__dirname, "views"));
+app.set("views engine", "ejs");
+
 indexRoute.get("/", (req, res) => {
-  res.render("index", { title: "Mini Messageboard", messages: messages });
+  res.render("index.ejs", { title: "Mini Message Board", messages: messages });
 });
 
 module.exports = indexRoute;
