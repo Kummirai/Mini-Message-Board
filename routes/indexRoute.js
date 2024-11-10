b116d48f0f8d29118f8f4b5e9fd9aac93aad9c1e
Kummirai <ajaxmilton@hotmail.com>
2024-11-10 17:34:36 +0200

    Create a view for root page

--- routes/indexRoute.js
+++ routes/indexRoute.js
@@ -1,8 +1,25 @@
 const { Router } = require("express");
 const indexRoute = Router();
+const path = require("node:path");
+
+app.set("views", path.join(__dirname, "views"));
+app.set("views engine", "ejs");
+
+const messages = [
+  {
+    text: "Hi, there",
+    user: "Amando",
+    added: new Date(),
+  },
+  {
+    text: "Hello, world",
+    user: "Charles",
+    added: new Date(),
+  },
+];
 
 indexRoute.get("/", (req, res) => {
-  res.send("Hello, wolrd from Node Js");
+  res.render("index", { title: "Mini Messageboard", messages: messages });
 });
 
 module.exports = indexRoute;
